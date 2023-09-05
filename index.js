25f35d0db0be178a05944e266beb174e6504601f
Kervin <kervinflerigene@gmail.com>
2023-09-04 20:16:07 -0400

    finishing nav

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
+const nav = document.getElementById('navbar');
+const navbar = document.getElementById('navbar')
 
 const navbarScroll = () => {
-      let nav = document.getElementById('navbar');
-      let navbar = document.getElementById('navbar')
 
       window.addEventListener('scroll', () => {
             if(window.scrollY){
                   nav.style.backgroundColor = 'black';
                   nav.style.transition = '0.6s';
-                  navbar.style.transition = '0.6s';
-                  navbar.style.position = 'fixed';
-                  navbar.style.top = '0px'
+                  // navbar.style.transition = '0.6s';
+                  nav.style.position = 'fixed';
+                  nav.style.top = '0px'
                   // console.log('yes')
             }else if(window.scrollY === 0){
                   nav.style.backgroundColor = 'transparent';
-                  navbar.style.position = 'relative';
-                  navbar.style.transition = '0.6s';
+                  nav.style.position = 'relative';
+                  // nav.style.transition = '0.6s';
                   // console.log(scrollY)
             }
             
       })
 }
 
+const removeJustifyContent = () => {
+      let navbarNav = document.getElementById('navbarNav');
+
+      // if(window.innerWidth < 991){
+      //       console.log('yes');
+      // }else{
+      //       console.log('no')
+      // }
+
+      // let p = 0
+
+     
+      // console.log(p)
+      
+}
+
 navbarScroll();
+removeJustifyContent();
 
 // Navbar scroll
